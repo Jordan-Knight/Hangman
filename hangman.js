bd606742ec66ab7e03ba34a241bb8a352d130953
Jordan Knight <jordanknight@Jordans-MBP.attlocal.net>
2017-04-29 19:13:49 -0500

    styling begin

--- hangman.js
+++ hangman.js
@@ -1,11 +1,25 @@
-var wordbank = ["item1", "item2", "item3", "item4", "item5"];
+var wordbank = ["tyrannosaurus", "triceratops", "velociraptor", "stegosaurus", "dilophosaurus"];
 var alpha = "qwertyuiopasdfghjklzxcvbnm";
 var counter = 10;
 var letterbank = [];
+var input = "";
 var word = "";
-var wordArray = [];
-var hyphenArray = [];
+var hyphens = "";
+var victory = 0;
+var wins = 0;
 
+document.onkeyup = function(event){
+
+    user = event.key;
+
+    if (alpha.indexOf(user) > -1) {
+        hangman.userInput(user);
+    } 
+
+    else {
+        alert("You gotta pick a letter bud. You pressed the " + user + " key.")
+    }
+}
 
 
 var hangman = {
@@ -14,84 +28,110 @@ var hangman = {
 		function (){
 			word = wordbank[Math.floor(Math.random()*wordbank.length)];
 			console.log(word);
+			victory = word.length;
 			
 		},
-	
-
-	letterArray : 
-
-		function (){
-			
-			for (i = 0; i < word.length; i++){
-
-				wordArray.push(word.charAt(i));
-			}
-
-			console.log(wordArray);
-		},
-	
 
 	hyphens : 
 		function (){
 			
-			for (i = 0; i < wordArray.length; i++){
-				hyphenArray.push("_");
+			for (i = 0; i < word.length; i++){
+				hyphens = hyphens + "_";
 			}
-			console.log(hyphenArray);
+			console.log(hyphens);
 		},
 
 	populate : 
 
 		function (){
-			document.getElementById("letters").innerHTML = hyphenArray;
+			document.getElementById("letters").innerHTML = hyphens;
 			document.getElementById("guess-counter").innerHTML = counter;
+			document.getElementById("win-counter").innerHTML = wins;
 
 		},
+
+
+    userInput: 
+
+	    function(key){
+	        if (input.indexOf(key) < 0) {
+
+	            if (word.indexOf(key) > -1) {
+	                this.guesser(key);
+
+	            } else {
+	                letterbank.push(key);
+	                document.getElementById("letter-bank").innerHTML = letterbank;
+	              	counter -= 1;
+	            	document.getElementById("guess-counter").innerHTML = counter;
+	            	if (counter === 0){
+	            		this.lose();
+	            	}
+
+	            }
+
+	            input = input + key;
+
+
+	        } 
+
+	    },
+
+	printWord: 
+
+		function(word){
+	        var nodeText = "";
+
+	        for (var i = 0; i < word.length; i++) {
+	            nodeText = nodeText + word[i] + "\t";
+	        }
+	        var div = document.getElementById("letters");
+	        div.innerText = nodeText;
+	    },
 
 	
 
 	guesser : 
 
-		function (){
-			document.onkeyup = function(event){
+		function (key){
+			var count = 0;
+	        var pos = 0;
 
-				console.log(event.key);
-				for (i = 0; i < wordArray.length; i++){
+	        while (word.indexOf(key, count) > -1) {
+	          var pos = word.indexOf(key, count)
+	          hyphens = hyphens.substring(0, pos) + key + hyphens.substring(pos + 1);
+	          console.log(hyphens);
+	          count = pos + 1;
+	          victory -= 1;
+	         }
 
-					if (event.key === wordArray[i]){
-						
-						hyphenArray[i] = wordArray[i];
-						document.getElementById("letters").innerHTML = hyphenArray;
-						return wordArray;
-					}
+		    this.printWord(hyphens);
 
-				}
-				if (event.key != wordArray[i] && alpha.indexOf(event.key) > -1){
-					counter--;
-					letterbank.push(event.key);
-					document.getElementById("guess-counter").innerHTML = counter;
-					document.getElementById("letter-bank").innerHTML = letterbank;
-				}
-				else {
+	        if (victory === 0) {
 
-			        alert("You gotta pick a letter bud.  You hit the " + event.key + " key.")
-			    }
-			}
+	        	this.win();
+	        }
+	            
+
 		},
-
-	gameover : {
-
-	},
 
 	setup : 
 
 		function(){
-			hangman.chooseWord();
-			hangman.letterArray();
-			hangman.hyphens();
-			hangman.populate();
-			hangman.guesser();
-			
+			this.chooseWord();
+			this.hyphens();
+			this.populate();
+
+		},
+
+	win :
+		function(){
+			console.log("win")
+			wins = wins ++;
+			document.getElementById("win-counter").innerHTML = wins;
+		},
+	lose :
+		function(){
 
 		},
 
